fb886ae1165814898f127f7ad1edcd0cb4593429
Tina Stanczyk <cstanczyk123@gmail.com>
2022-01-06 12:57:10 -0500

    first commit

---
+++ generateMarkdown.js
@@ -0,0 +1,68 @@
+// create function to render markdown
+function generateMarkdown(data){
+  return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>${data.username}'s Portfolio Page</title>
+  </head>
+  <body>
+  <header>
+    <h1>${data.username}'s Portfolio Page</h1>
+    <nav>
+      <ul>
+        <li><a href="#About">About</a></li>
+        <li><a href="#Projects">Projects</a></li>
+        <li><a href="#Contacts">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+  
+  <main>
+    <section>
+      <h2 id="About">About</h2>
+      <h3>${data.username}</h3>
+      <p>${data.bio}</p>
+      <p>${data.location}</p>
+    </section>
+    <section>
+      <h2 id="Projects">Projects</h2>
+      <article>
+        <h3>Project 1</h3>
+        <p>Description of Project 1</p>
+      </article>
+      <article>
+        <h3>Project 2</h3>
+        <p>Description of Project 2</p>
+      </article>
+      <article>
+        <h3>Project 3</h3>
+        <p>Description of Project 3</p>
+      </article>
+      <article>
+        <h3>Project 4</h3>
+        <p>Description of Project 4</p>
+      </article>
+      <article>
+        <h3>Project 5</h3>
+        <p>Description of Project 5</p>
+      </article>
+    </section>
+    <section>
+      <h2 id="Contacts">Contact</h2>
+      <h3><a href="${data.linkedIn}">LinkedIn Profile</a></h3>
+      <h3><a href="https://github.com/${data.github}">Github Profile</a></h3>
+    </section>
+  </main>
+  
+  
+  
+    <script src="index.js"></script>
+  </body>
+  </html>`
+}
+
+// create module to export markdown
+module.exports = generateMarkdown;
---
+++ index.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const markDown = require('./generateMarkdown');
+
+// create array of questions
+
+const questions = [
+  {
+    type: 'input',
+    message: 'What is your full name?',
+    name: 'username',
+  },
+  {
+    type: 'input',
+    message: 'Where are you located?',
+    name: 'location',
+  },
+  {
+    type: 'input',
+    message: 'Write a short description about yourself.',
+    name: 'bio',
+  },
+  {
+    type: 'input',
+    message: 'Paste your LinkedIn Profile Link.',
+    name: 'linkedIn',
+  },
+  {
+    type: 'input',
+    message: 'What is your Github username?',
+    name: 'github',
+  },
+];
+
+// create function to write file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, markDown(data), (err) =>
+     err ? console.error(err) : console.log("Success!")
+   );
+ }
+
+// create function to initiate application
+ function init() {
+  inquirer
+    .prompt(questions)
+    .then((response) => {
+      // generating new readme in separate folder so it doesn't overwrite the application's readme
+      writeToFile("./assets/index.html", response)
+  });
+}
+// call function that initiates application
+init();
